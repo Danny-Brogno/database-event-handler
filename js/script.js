3dbf54fc9e260cd22a0db26a5be4611b1f7e342c
Danny <danny.brogno@gmail.com>
2021-09-17 19:20:33 +0200

    milestone 1 done

--- js/script.js
+++ js/script.js
@@ -7,87 +7,107 @@ function vueInit() {
 
     data: {
 
-      "collection": [],
-      "select": "All",
+      "file": null,
+      "content": null
 
-      "dataBaseInfo": [
-        {
-          "eventType": "INSERTED",
-          "entityName": "PLACEMENT",
-          "entityId": 11,
-          "fieldsUpdated":
-          [
-            "null"
-          ]
-        },
-        {
-          "eventType": "UPDATED",
-          "entityName": "PLACEMENT",
-          "entityId": 22,
-          "fieldsUpdated":
-          [
-            "status",
-            "hoursPerDay"
-          ]
-        },
-        {
-          "eventType": "DELETED",
-          "entityName": "COMPANY",
-          "entityId": 33,
-          "fieldsUpdated":
-          [
-            "status",
-            "companyUrl"
-          ]
-        },
-        {
-          "eventType": "INSERTED",
-          "entityName": "PLACEMENT",
-          "entityId": 44,
-          "fieldsUpdated":
-          [
-            "null"
-          ]
-        },
-        {
-          "eventType": "UPDATED",
-          "entityName": "COMPANY",
-          "entityId": 55,
-          "fieldsUpdated":
-          [
-            "status",
-            "companyUrl",
-            "hoursPerDay"
-          ]
-        },
-        {
-          "eventType": "DELETED",
-          "entityName": "PLACEMENT",
-          "entityId": 66,
-          "fieldsUpdated":
-          [
-            "null"
-          ]
-        },
-        {
-          "eventType": "INSERTED",
-          "entityName": "COMPANY",
-          "entityId": 77,
-          "fieldsUpdated":
-          [
-            "status",
-            "companyUrl",
-            "hoursPerDay",
-            "overtimeRate"
-          ]
-        }
-      ]
+      // "dataBaseInfo": [
+      //   {
+      //     "eventType": "INSERTED",
+      //     "entityName": "PLACEMENT",
+      //     "entityId": 11,
+      //     "fieldsUpdated":
+      //     [
+      //       "null"
+      //     ]
+      //   },
+      //   {
+      //     "eventType": "UPDATED",
+      //     "entityName": "PLACEMENT",
+      //     "entityId": 22,
+      //     "fieldsUpdated":
+      //     [
+      //       "status",
+      //       "hoursPerDay"
+      //     ]
+      //   },
+      //   {
+      //     "eventType": "DELETED",
+      //     "entityName": "COMPANY",
+      //     "entityId": 33,
+      //     "fieldsUpdated":
+      //     [
+      //       "status",
+      //       "companyUrl"
+      //     ]
+      //   },
+      //   {
+      //     "eventType": "INSERTED",
+      //     "entityName": "PLACEMENT",
+      //     "entityId": 44,
+      //     "fieldsUpdated":
+      //     [
+      //       "null"
+      //     ]
+      //   },
+      //   {
+      //     "eventType": "UPDATED",
+      //     "entityName": "COMPANY",
+      //     "entityId": 55,
+      //     "fieldsUpdated":
+      //     [
+      //       "status",
+      //       "companyUrl",
+      //       "hoursPerDay"
+      //     ]
+      //   },
+      //   {
+      //     "eventType": "DELETED",
+      //     "entityName": "PLACEMENT",
+      //     "entityId": 66,
+      //     "fieldsUpdated":
+      //     [
+      //       "null"
+      //     ]
+      //   },
+      //   {
+      //     "eventType": "INSERTED",
+      //     "entityName": "COMPANY",
+      //     "entityId": 77,
+      //     "fieldsUpdated":
+      //     [
+      //       "status",
+      //       "companyUrl",
+      //       "hoursPerDay",
+      //       "overtimeRate"
+      //     ]
+      //   }
+      // ] // END of databaseInfo
 
     }, // END of DATA
 
     methods: {
 
-    }// END of METHODS
+      readFile() {
+        this.file = this.$refs.doc.files[0];
+        const reader = new FileReader();
+        if (this.file.name.includes(".txt")) {
+          reader.onload = (res) => {
+            this.content = res.target.result;
+          };
+          reader.onerror = (err) => console.log(err);
+          reader.readAsText(this.file);
+        } else {
+          this.content = "check the console for file output";
+          reader.onload = (res) => {
+            console.log(res.target.result);
+          };
+          reader.onerror = (err) => console.log(err);
+          reader.readAsText(this.file);
+        }
+        
+      } // END of readFile() function
+
+    } // END of METHODS
 
   }) // END NEW VUE
 
