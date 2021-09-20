dadc74e576c8fe5402a8f3d2a897636bf0ba480d
Danny <danny.brogno@gmail.com>
2021-09-20 18:04:14 +0200

    checkpoint

--- js/script.js
+++ js/script.js
@@ -21,13 +21,12 @@ function vueInit() {
           reader.onload = (res) => {
             this.content = res.target.result;
 
-
             const textLine = this.content.split("\n"); // JAVA language that tells the split to stop when it encounters the end of the line
             // let arrayIntoObj = ({...textLine}); // Spread syntax (...) allows an iterable such as an array expression or string to be expanded in places where zero or more arguments (for function calls) or elements (for array literals) are expected, or an object expression to be expanded in places where zero or more key-value pairs (for object literals) are expected.
 
             for (i = 0; i < textLine.length; i++) {
-              let iesimo = textLine[i];
-              console.log(iesimo.split(","));
+              let singleElement = textLine[i];
+              console.log(singleElement.split(","));
             }
 
           };
